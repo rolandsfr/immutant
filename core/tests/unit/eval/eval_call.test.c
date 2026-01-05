2f0e61e7d33d66656398035430ee8593f9975b1c
Rolands Fridemanis <rolands.affaires@gmail.com>
2026-01-05 10:15:01 +0200

    test converage for purity semantics

--- core/tests/unit/eval/eval_call.test.c
+++ core/tests/unit/eval/eval_call.test.c
@@ -41,7 +41,7 @@ Value mock_function(ValueBuffer* arguments, Context* context)
 
 void test_eval_call_should_invoke_function_with_arguments(void)
 {
-	Env* env = env_new(NULL);
+	Env* env = env_new(NULL, ENV_IMPURE);
 	define_mock_function_in_env(env, "mockFunc", 1, mock_function, IMPURE);
 
 	ArgumentsArray args;
@@ -67,7 +67,7 @@ void test_eval_call_should_invoke_function_with_arguments(void)
 
 void test_eval_call_should_return_error_when_callee_is_not_function(void)
 {
-	Env* env = env_new(NULL);
+	Env* env = env_new(NULL, ENV_IMPURE);
 
 	ArgumentsArray args;
 	ArgumentsArray_init(&args);
@@ -90,7 +90,7 @@ void test_eval_call_should_return_error_when_callee_is_not_function(void)
 
 void test_eval_call_should_return_error_on_incorrect_argument_count(void)
 {
-	Env* env = env_new(NULL);
+	Env* env = env_new(NULL, ENV_IMPURE);
 	define_mock_function_in_env(env, "mockFunc", 2, mock_function, IMPURE);
 
 	ArgumentsArray args;
@@ -110,6 +110,70 @@ void test_eval_call_should_return_error_on_incorrect_argument_count(void)
 
 	Value result = eval_call(call_expr, &error, env);
 	TEST_ASSERT_EQUAL_INT(RUNTIME_INCORRECT_ARG_COUNT, error.type);
+
+	ArgumentsArray_free(&args);
+}
+
+void test_eval_call_should_return_error_when_pure_function_calls_impure(void)
+{
+	Env* env = env_new(NULL, ENV_PURE);
+	define_mock_function_in_env(env, "impureFunc", 1, mock_function, IMPURE);
+
+	ArgumentsArray args;
+	ArgumentsArray_init(&args);
+	ArgumentsArray_push(&args, NULL);
+
+	VariableExpr* callee_expr = make_variable_expr("impureFunc", 1);
+	CallExpr* call_expr = make_call_expr((Expr*)callee_expr, args.data, 1, 1);
+	Error error = {-1};
+
+	// mock identifier evaluation
+	Value* callee = env_get(env, "impureFunc");
+	eval_expr_ExpectAndReturn((Expr*)callee_expr, &error, env, *callee);
+
+	// mock argument evaluation
+	eval_expr_ExpectAndReturn(call_expr->args[0], &error, env, make_number(10));
+
+	Value result = eval_call(call_expr, &error, env);
+	TEST_ASSERT_EQUAL_INT(RUNTIME_PURITY_VIOLATION, error.type);
+
+	ArgumentsArray_free(&args);
+}
+
+void test_pure_function_should_not_resolve_mutable_variable_from_outer_scope(
+	void)
+{
+	Env* global_env = env_new(NULL, ENV_IMPURE);
+	define_mock_function_in_env(global_env, "impureFunc", 1, mock_function,
+								IMPURE);
+
+	// Define a mutable variable in the global environment
+	env_define_var(global_env, "mutableVar", make_number(5), MUTABLE);
+
+	ArgumentsArray args;
+	ArgumentsArray_init(&args);
+	ArgumentsArray_push(&args, NULL);
+
+	// create impure function
+	VariableExpr* callee_expr = make_variable_expr("impureFunc", 1);
+	CallExpr* call_expr = make_call_expr((Expr*)callee_expr, args.data, 1, 1);
+	Error error = {-1};
+
+	// mock identifier evaluation
+	Value* callee = env_get(global_env, "impureFunc");
+	eval_expr_ExpectAndReturn((Expr*)callee_expr, &error, global_env, *callee);
+
+	// mock argument evaluation to try to access mutableVar
+	VariableExpr* arg_expr = make_variable_expr("mutableVar", 1);
+	call_expr->args[0] = (Expr*)arg_expr;
+
+	error.type = RUNTIME_UNDEFINED_VARIABLE; // Simulate the error
+
+	Value result = eval_call(call_expr, &error, global_env);
+	TEST_ASSERT_EQUAL_INT(
+		RUNTIME_UNDEFINED_VARIABLE,
+		error.type); // mutable var is defined globally, but should not be
+					 // accessible in pure function
 
 	ArgumentsArray_free(&args);
 }
